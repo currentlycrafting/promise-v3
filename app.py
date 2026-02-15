5fe2494b4a2bd76e2a6a6bf5088e934f62d75912
Khalid <khalidmuse612@gmail.com>
2026-02-14 21:35:21 -0600

    timer + y/n logic for promise checker

--- app.py
+++ app.py
@@ -1,25 +1,52 @@
 from gemini_client import refine_promise 
-
-
-
+import time 
 
 promise = input("Enter a promise: ") #"I want to work out for 2 hours eevry single day"
+print("Choose a timer:")
+print("1. 5 seconds")
+print("2. 10 seconds")
+print("3. 15 seconds")
+promise_time = input("Enter 1,2, or 3: ")
 
 
-issue =input("Enter the issue you currently have with acheiving this promise: ") #"I have too many things in my agenda"
-pre_category = input(f"Pick which category you feel like this issue is apart of. Choose 1,2,or 3: \n 1. Time \n 2. Lack of Resource(s) \n 3. Too much friction \n ")
-
-if int(pre_category) == 1:
-    category = "Time"
-elif int(pre_category) == 2:
-    category = "Lack of Resource(s)"
-elif int(pre_category) == 3:
-    category = "Too much friction"
+if promise_time == "1":
+    seconds = 5
+elif promise-time == "2":
+    seconds = 10
+elif promise-time == "3":
+    seconds = 15
 else:
-    print('Not a valid option')
+    print('Invalid Choice')
+    exit()
 
 
-print(refine_promise(promise, issue, category))
+time.sleep(seconds)
+
+update = input("Did you keep your promise?(Y/N): ")
+
+if update == "n" or update == "N":
+    issue =input("Enter the issue you currently have with acheiving this promise: ") #"I have too many things in my agenda"
+    print("Pick which category you feel like this issue is apart of. Choose 1,2,or 3: \n 1. Time \n 2. Lack of Resource(s) \n 3. Too much friction \n ")
+    pre_category = input("Enter 1,2, or 3: ")
+
+    if int(pre_category) == 1:
+        category = "Time"
+    elif int(pre_category) == 2:
+        category = "Lack of Resource(s)"
+    elif int(pre_category) == 3:
+        category = "Too much friction"
+    else:
+        print('Not a valid option')
+    print(refine_promise(promise, issue, category))
+
+elif update == "y" or update == "Y":
+    print("Promise kept!")
+
+else:
+    print('Invalid option')
+
+
+
 
 
 
